26803948f9ce428cac451d117e2a45e112cc3ae4
Creepr6 <Warre088>
2018-09-27 09:34:48 +0200

    oppervlakte cirkel afgewerkt

---
+++ 04 - Variabelen/oppervlakte cirkel.py
@@ -0,0 +1,7 @@
+r = float(input(' geef straal: '))
+pi = 3.14159
+opp_cirkel = pi * (r**2)
+# De oppervlakte van een cirkel met straal 2.0 is 12.56636
+resultaat = 'De oppervlakte van een cirkel met straal '
+resultaat += str(r) + ' is ' + str(opp_cirkel)
+print(resultaat)
--- 04 - Variabelen/test.py
+++ 04 - Variabelen/test.py
@@ -1,9 +1,4 @@
-# coefficienten van x^2, x^1 en x^0
-a = float(input('geef a: '))
-b = float(input('geef b: '))
-c = float(input('geef c: '))
-print('a=', a, ', b=', b, ', c=', c)
-# discriminant berekenen
-d = ( b**2 ) - (4 * a * c)
-# discriminant tonen
-print(d)
+aantal_leerlingen_in_klas = 18
+nieuwe_leerlingen = int(input('aantal nieuwe leerlingen: '))
+aantal_leerlingen_in_klas += nieuwe_leerlingen
+print(aantal_leerlingen_in_klas)
