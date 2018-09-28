b54ae95ad2f7d1c2f950076234b62720aa804130
Creepr6 <Warre088>
2018-09-28 22:59:41 +0200

    constante van avogrado afgewerkt

--- 04 - Variabelen/Constante van Avogrado.py
+++ 04 - Variabelen/Constante van Avogrado.py
@@ -1,4 +1,7 @@
-n = float(input('aantal deeltjes S: ')
+n = float(input('aantal deeltjes S: '))
 Na = 6.020 * 10**23
 mx = 32.06
-resultaat = (n * mx)
+resultaat1 = (n * mx)
+resultaat = (n * Na )
+print(resultaat1)
+print(resultaat)
