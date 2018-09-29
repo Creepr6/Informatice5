0bdeb0fbe6b75e3ca51b4e5a29fc384ac70b4584
Creepr6 <Warre088>
2018-09-29 16:19:58 +0200

    wisselgeld afgewerkt

--- 04 - Variabelen/wisselgeld.py
+++ 04 - Variabelen/wisselgeld.py
@@ -1,2 +1,4 @@
-a = float(input('bedrag = '))
-print('f(x)' + ' = ' + 'str(2(x - 3)**2 + 4)')
+a = float(input(' bedrag = '))
+resultaat = str(a) + ' cent kan je omwisselen in ' + str(b) + ' muntstukken'
+b = (a//100) + ((a%100)//50) + (((a%100)%50)//20) + ((((a%100)%50)%20)//10) + (((((a%100)%50)%20)%10)//5) + ((((((a%100)%50)%20)%10)%5)//2) + (((((((a%100)%50)%20)%10)%5)%2)//1)
+print(resultaat)
