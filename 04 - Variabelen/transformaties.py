ee741e859712f187ac0baee8d29b789e2b0dd3f5
Creepr6 <Warre088>
2018-09-29 15:12:37 +0200

    transformaties afgewerkt

---
+++ 04 - Variabelen/transformaties.py
@@ -0,0 +1,4 @@
+a = float(input('a = '))
+b = float(input('b = '))
+print('f(x)' + ' = ' + str(2) + '(x ' + '- ' + str(3) + ')' + '^' + str(2) + ' + ' + str(4))
+print('g(x)' + ' = ' + str(2) + '(x ' + '- ' + str(int(3 + a)) + ')' + '^' + str(2) + ' + ' + str(int(4 + b)))
---
+++ 04 - Variabelen/wisselgeld.py
@@ -0,0 +1,2 @@
+a = float(input('bedrag = '))
+print('f(x)' + ' = ' + 'str(2(x - 3)**2 + 4)')
