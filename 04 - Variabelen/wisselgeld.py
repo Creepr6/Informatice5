fc8d8873d88a3eb8656cfd31c3dc665e2a11ac0a
Creepr6 <Warre088>
2018-10-01 14:36:58 +0200

    wisselgeld afgewerkt

--- 04 - Variabelen/wisselgeld.py
+++ 04 - Variabelen/wisselgeld.py
@@ -1,4 +1,12 @@
-a = float(input(' bedrag = '))
-resultaat = str(a) + ' cent kan je omwisselen in ' + str(b) + ' muntstukken'
-b = (a//100) + ((a%100)//50) + (((a%100)%50)//20) + ((((a%100)%50)%20)//10) + (((((a%100)%50)%20)%10)//5) + ((((((a%100)%50)%20)%10)%5)//2) + (((((((a%100)%50)%20)%10)%5)%2)//1)
-print(resultaat)
+a = int(input(' bedrag = '))
+r1 = a // 100
+r2 = (a - (r1 * 100)) // 50
+r3 = (a - (r1 * 100) - (r2 * 50)) // 20
+r4 = (a - (r1 * 100) - (r2 * 50) - (r3 * 20)) // 10
+r5 = (a - (r1 * 100) - (r2 * 50) - (r3 * 20) - (r4 * 10)) // 5
+r6 = (a - (r1 * 100) - (r2 * 50) - (r3 * 20) - (r4 * 10) - (r5 * 5)) // 2
+r7 = (a - (r1 * 100) - (r2 * 50) - (r3 * 20) - (r4 * 10) - (r5 * 5) - (r6 * 2)) // 1
+resultaat = int(r5 + r1 + r2 + r3 + r4 + r6 + r7)
+antwoord = str(a) + ' cent kan je omwisselen in ' + str(resultaat) + ' muntstukken'
+print(antwoord)
+
