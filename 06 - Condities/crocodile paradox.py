caa9eedcfb25e826a0677c3d9fd303751059ddc8
Creepr6 <Warre088>
2018-10-11 09:50:30 +0200

    crocodile paradox afgewerkt

---
+++ 06 - Condities/crocodile paradox.py
@@ -0,0 +1,7 @@
+a = input('wie heeft stalen zenuwen?: ')
+if a == 'vader':
+    print('Krokodil geeft kind terug')
+elif a == 'krokodil':
+    print('Krokodil eet kind op')
+else:
+    print('Moe van het denken')
