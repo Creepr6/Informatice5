17e631030bf3088aefef8750a871b0a139fbd1c3
Creepr6 <Warre088>
2018-11-11 19:19:40 +0100

    het grootste en het gemiddelde afgewerkt

---
+++ 07a - Iteraties-For-Ius/Het grootste en het gemiddelde.py
@@ -0,0 +1,25 @@
+#invoer
+kaas = int(input('aantal getallen:'))
+
+#berekenen
+x = 0
+grootste = -10000000000000000000000
+for i in range(kaas):
+   getal = int(input('getal: '))
+   x += getal
+   gemiddelde = (x / kaas)
+   if getal > grootste:
+      grootste = getal
+
+#antwoorden
+antwoord = 'Het grootste getal is ' + str(grootste) + ' en het gemiddelde is ' + '{:.2f}'.format(gemiddelde)
+
+#uitvoer
+print(antwoord)
+
+
+
+
+
+
+
