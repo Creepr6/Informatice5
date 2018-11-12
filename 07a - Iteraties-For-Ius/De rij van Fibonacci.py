09b04d14e834c5434e2b9076b990f9d45cdce20b
Creepr6 <Warre088>
2018-11-12 19:43:55 +0100

    rij van fibonacci afgewerkt

--- 07a - Iteraties-For-Ius/De rij van Fibonacci.py
+++ 07a - Iteraties-For-Ius/De rij van Fibonacci.py
@@ -1,1 +1,15 @@
 n = int(input('een getal n∈N0: '))
+
+if n == 1 or n == 2:
+    print(str(1))
+elif n == 0:
+    print(str(0))
+else:
+    def fib(n):
+        a = 1
+        b = 1
+        for _ in range (n-1):
+            a,b = b, a+b
+        return a
+    print(fib(n))
+
