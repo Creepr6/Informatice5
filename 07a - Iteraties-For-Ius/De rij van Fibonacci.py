8d964c5b3c82ed0011a6205a8570a4e9aba33bac
Creepr6 <Warre088>
2018-11-15 09:35:53 +0100

    veiling afgewerkt

--- 07a - Iteraties-For-Ius/De rij van Fibonacci.py
+++ 07a - Iteraties-For-Ius/De rij van Fibonacci.py
@@ -1,3 +1,4 @@
+#mijnen
 n = int(input('een getal n∈N0: '))
 
 if n == 1 or n == 2:
@@ -12,4 +13,14 @@ else:
             a,b = b, a+b
         return a
     print(fib(n))
+
+#inde les
+n = int(input('hoeveelste getal van Fibonacci: '))
+
+vorige, huidige = 1,1
+
+for i in range(n-2):
+    vorige, huidige = huidige, huidige + vorige
+
+print(huidige)
 
---
+++ 07a - Iteraties-For-Ius/test 2.py
@@ -0,0 +1,20 @@
+getal = 0
+while getal < 5:
+    print(getal)
+    #oneindige lus
+
+getal = 0
+while getal < 5:
+    print(getal)
+    getal +=1
+
+for i in range(0, 5):
+    print(i)
+
+vorst_periode = 0
+temperatuur = int(input('Dagtemperatuur: '))
+while temperatuur <= 0:
+ vorst_periode += 1
+ temperatuur = int(input('Dagtemperatuur: '))
+print(vorst_periode)
+
