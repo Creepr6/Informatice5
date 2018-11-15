963f76a247da7dc6677a686bca2fcae432b72801
Creepr6 <Warre088>
2018-11-15 09:37:02 +0100

    vorstperiode afgewerkt

--- 07b - Iteraties - While-lus/vorstperiode.py
+++ 07b - Iteraties - While-lus/vorstperiode.py
@@ -1,7 +1,7 @@
-
 vorst_periode = 0
 temperatuur = int(input('Dagtemperatuur: '))
+
 while temperatuur <= 0:
- vorst_periode += 1
- temperatuur = int(input('Dagtemperatuur: '))
+         vorst_periode += 1
+         temperatuur = int(input('Dagtemperatuur: '))
 print(vorst_periode)
