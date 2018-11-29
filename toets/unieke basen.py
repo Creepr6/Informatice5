d427df7c78b9dd787bc7e89c89f53f2abc5c6ca3
Creepr6 <Warre088>
2018-11-29 09:00:18 +0100

    unieke basen afgewerkt

---
+++ toets/unieke basen.py
@@ -0,0 +1,21 @@
+#invoer
+x = int(input('aantal basen: '))
+woord = ''
+aantal_basen = 0
+
+#berekenen
+for i in range(x):
+    base = input('getal: ')
+    if base in 'ATCG' and base not in woord:
+        aantal_basen += 1
+        woord += base + ' '
+
+#antwoorden
+if aantal_basen == 1:
+    antwoord = 'De DNA-keting bevat ' + str(aantal_basen) + ' soort base: ' + woord
+
+elif aantal_basen > 1:
+    antwoord = 'De DNA-keting bevat ' + str(aantal_basen) + ' verschillende soorten basen: ' + woord
+
+#uitvoer
+print(str(antwoord))
