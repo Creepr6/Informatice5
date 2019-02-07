45779020961e2252b2b7ba347078584350e69371
Creepr6 <Warre088>
2019-02-07 08:43:30 +0100

    vlaggen afgewerkt

--- 11 - Tuples/schaakverslag.py
+++ 11 - Tuples/schaakverslag.py
@@ -20,6 +20,25 @@ def geldige_zetten(zet):
             kaas += 1
     return kaas == len(zet)
 
+#in de les:
+def geldige_zet2(zet):
 
+    #controleren of index 0 in K,L...
+    if len(zet) == 3 and zet[0] in 'KDTLP' and zet[1] in 'abcdefgh' and zet[2] in '12345678':
+        mes = True
+    elif len(zet) == 2 and zet[0] in 'abcdefgh' and zet[1] in '12345678':
+        mes = True
+    else:
+        mes = False
+    return mes
+
+def geldige_zetten2(zetten):
+
+    i = 0
+
+    while i < len(zetten) and geldige_zet2(zetten[i]):
+        i += 1
+
+    return i >= len(zetten)
 
 
