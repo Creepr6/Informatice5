4136c7d4ec5a582b3ac9d40c28615ced41832d70
Creepr6 <Warre088>
2019-02-14 09:32:10 +0100

    snoepwinkel afgewerkt

---
+++ 12b - roosters/snoepwinkel.py
@@ -0,0 +1,20 @@
+def bereken_prijs(boodschappenlijst):
+    prijs = 0
+    for item in boodschappenlijst:
+        prijs += item[1]
+
+    return prijs
+
+
+def winkelbriefje(boodschappenlijst):
+    producten = []
+    for item in boodschappenlijst:
+        producten.append(item[0])
+
+    return producten
+
+def sorteer(boodschappenlijst):
+    from operator import itemgetter
+    boodschappenlijst.sort(key=itemgetter(1))
+
+    return boodschappenlijst
